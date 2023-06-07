cdd22b40a56f6976921386efcd424a60e9a2d291
bharath678 <131230212+bharath678@users.noreply.github.com>
2023-06-08 02:56:22 +0530

    Add files via upload

---
+++ animationJS.js
@@ -0,0 +1,10 @@
+$("button").on("click" ,function () {
+  // $("h1").toggle();
+  setTimeout($(".dead").fadeToggle(),200);
+
+  // $("h1").fadeOut();
+  // $("h1").fadeIn();
+  // $("h1").hide();
+  // $("h1").show();
+
+})
